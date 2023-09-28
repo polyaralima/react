9c7eeba9f8ca96878c166b503604dda3778fc415
polyaralima <polly-kelly-2011@hotmail.com>
2023-09-28 16:21:11 -0300

    react

---
+++ src/componentes/app.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PrimeiroProps from "./basico/PrimeiroProps";
+
+export default function App(){
+    return(
+        <div>
+            <PrimeiroProps titulo1= 'titulo1' titulo2 = 'titulo2'/>
+        </div>
+    )
+}
---
+++ src/componentes/basico/PrimeiroProps.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default (props) => {
+    return(
+        <div>
+            <h1>{props.titulo1}</h1>
+            <h2>{props.titulo2}</h2>
+        </div>
+    )
+
+}
